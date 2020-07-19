7cdf15a079c2ee3ce0d9be7a91853dae3a003e12
Pavol Podstreleny <palo.podstreleny@gmail.com>
2020-07-19 06:25:22 +0100

    Transform pricing service to microservice and
    Implement Eureka server

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -1,17 +1,51 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.domain.price.PriceRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
- * TODO: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PricingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PricingServiceApplication.class, args);
     }
+
+    /**
+     * Initializes the prices available in the pricing-service.
+     * @param repository where the pricing information persists.
+     * @return the car manufacturers to add to the related repository
+     */
+    @Bean
+    CommandLineRunner initDatabase(PriceRepository repository) {
+        return args -> {
+            for (long i = 1; i <= 20; i++){
+                repository.save(new Price("USD",randomPrice(), i));
+            }
+        };
+    }
+
+    /**
+     * Gets a random price to fill in for a given vehicle ID.
+     * @return random price for a vehicle
+     */
+    private static BigDecimal randomPrice() {
+        return new BigDecimal(ThreadLocalRandom.current().nextDouble(1, 5))
+                .multiply(new BigDecimal(5000d)).setScale(2, RoundingMode.HALF_UP);
+    }
+
+
 
 }
--- pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,14 +1,22 @@
 package com.udacity.pricing.domain.price;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
 
     private String currency;
     private BigDecimal price;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long vehicleId;
 
     public Price() {
@@ -18,6 +26,11 @@ public class Price {
         this.currency = currency;
         this.price = price;
         this.vehicleId = vehicleId;
+    }
+
+    public Price(String currency, BigDecimal price){
+        this.currency = currency;
+        this.price = price;
     }
 
     public String getCurrency() {
